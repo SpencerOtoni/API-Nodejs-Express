0625157692829629c476299c2a8d85816e21236d
SpencerOtoni <sspencerotoni@gmail.com>
2021-02-04 17:15:52 -0300

    Relacionamento entre tabelas

--- src/database/tabelas.js
+++ src/database/tabelas.js
@@ -7,21 +7,30 @@ class Tabelas {
         console.log('tabela')
     }
 
-    criarAtendimento() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+    criarUser(){
+        const sql = 'CREATE TABLE IF NOT EXISTS User(id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), nome varchar(50), senha varchar(20), email varchar(50))'
 
         query.executaQuery(sql)
-            .then(console.log('Tabela Atendimentos criada com sucesso'))
+            .then(console.log('Tabela User criada com sucesso'))
             .catch(erro => console.log(erro))
     }
 
     criarPets(){
-        const sql = 'CREATE TABLE IF NOT EXISTS Pets(id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), nome varchar(50), imagem varchar(200))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Pets(id int NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), nome varchar(50), imagem varchar(200), idUser int NOT NULL, FOREIGN KEY (idUser) REFERENCES User (id))'
 
         query.executaQuery(sql)
             .then(console.log('Tabela Pets criada com sucesso'))
             .catch(erro => console.log(erro))
     }
+
+    criarAtendimento() {
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id), idPet int NOT NULL, FOREIGN KEY (idPet) REFERENCES Pets (id))'
+
+        query.executaQuery(sql)
+            .then(console.log('Tabela Atendimentos criada com sucesso'))
+            .catch(erro => console.log(erro))
+    }
+   
 }
 
 export default new Tabelas()
