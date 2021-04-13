ef920f3da6c4098340028b0c2a7d240199f4034e
SpencerOtoni <sspencerotoni@gmail.com>
2021-04-13 08:26:15 -0300

    Populando banco

--- Teste-godevapi/src/app/controllers/PopularConnectorController.js
+++ Teste-godevapi/src/app/controllers/PopularConnectorController.js
@@ -1,4 +1,3 @@
-import path from 'path'
 import Connector from '../models/connector'
 
 import AppError from '../errors/AppError'
@@ -6,85 +5,71 @@ import AppError from '../errors/AppError'
 class PopularConnectorController {
     async store(req, res) {
         try {
-            return res.status(201).json({
-                name: 'Conector 01',
-                category: 'Prémio',
-                description: 'Descrição conector 01',
-                type: 'REST',
-                privacy: 'PUBLIC',
-                logo_URL: path.resolve(
-                    __dirname,
-                    '..',
-                    '..',
-                    '..',
-                    'tmp',
-                    'uploads'
-                ),
-                base_URL: process.env.APP_URL,
-            })
+            const connectors = [
+                {
+                    name: 'Conector 01',
+                    category: 'Prémio',
+                    description: 'Descrição conector 01',
+                    type: 'REST',
+                    privacy: 'PUBLIC'.
+                    logo_URL: '',
+                    base_URL: process.env.APP_URL,
+                },
+                {
+                    name: 'Conector 02',
+                    category: 'Básico',
+                    description: 'Descrição conector 02',
+                    type: 'BD',
+                    privacy: 'PUBLIC'.
+                    logo_URL: '',
+                    base_URL: process.env.APP_URL,
+                },
+                {
+                    name: 'Conector 03',
+                    category: 'Básico',
+                    description: 'Descrição conector 03',
+                    type: 'SOAP',
+                    privacy: 'PRIVATE'.
+                    logo_URL: '',
+                    base_URL: process.env.APP_URL,
+                },
+                {
+                    name: 'Conector 04',
+                    category: 'Prémio',
+                    description: 'Descrição conector 04',
+                    type: 'REST',
+                    privacy: 'PRIVATE'.
+                    logo_URL: '',
+                    base_URL: process.env.APP_URL,
+                },
+                {
+                    name: 'Conector 05',
+                    category: 'Básico',
+                    description: 'Descrição conector 05',
+                    type: 'BD',
+                    privacy: 'PRIVATE'.
+                    logo_URL: '',
+                    base_URL: process.env.APP_URL,
+                },
+                {
+                    name: 'Conector 06',
+                    category: 'Básico',
+                    description: 'Descrição conector 06',
+                    type: 'SOAP',
+                    privacy: 'PUBLIC'.
+                    logo_URL: '',
+                    base_URL: process.env.APP_URL,
+                }
+        
+            ]
+
+            const connectors = await Connector.create([])
+
+            return res.status(201).json(connectors)
         } catch (err) {
             throw new AppError(err)
         }
-        /*
-         {
-            name: 'Conector 01',
-            category: 'Prémio',
-            description: 'Descrição conector 01',
-            type: 'REST',
-            privacy: 'PUBLIC'.
-            logo_URL: path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'),
-            base_URL: process.env.APP_URL,
-        },
-        {
-            name: 'Conector 02',
-            category: 'Básico',
-            description: 'Descrição conector 02',
-            type: 'BD',
-            privacy: 'PUBLIC'.
-            logo_URL: path,
-            base_URL: process.env.APP_URL,
-        },
-        {
-            name: 'Conector 03',
-            category: 'Básico',
-            description: 'Descrição conector 03',
-            type: 'SOAP',
-            privacy: 'PRIVATE'.
-            logo_URL: path,
-            base_URL: process.env.APP_URL,
-        },
-        {
-            name: 'Conector 04',
-            category: 'Prémio',
-            description: 'Descrição conector 04',
-            type: 'REST',
-            privacy: 'PRIVATE'.
-            logo_URL: path,
-            base_URL: process.env.APP_URL,
-        },
-        {
-            name: 'Conector 05',
-            category: 'Básico',
-            description: 'Descrição conector 05',
-            type: 'BD',
-            privacy: 'PRIVATE'.
-            logo_URL: path,
-            base_URL: process.env.APP_URL,
-        },
-        {
-            name: 'Conector 06',
-            category: 'Básico',
-            description: 'Descrição conector 06',
-            type: 'SOAP',
-            privacy: 'PUBLIC'.
-            logo_URL: path,
-            base_URL: process.env.APP_URL,
-        }
 
-        const connectors = await Connector.create([])
-
-        return res.status(201).json(connectors)
-        */
     }
 }
 
